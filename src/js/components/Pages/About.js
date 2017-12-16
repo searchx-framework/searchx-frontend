78a1c5fbb285a2f88fdfc77eeae9c68d5e17b328
sindunuragarp <sindunuragarp@gmail.com>
2017-12-16 22:56:23 +0100

    Added example socket connection in about

--- src/js/components/Pages/About.js
+++ src/js/components/Pages/About.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './About.css'
 
+import { subscribeToTimer } from '../../stores/SocketStore';
+
 class About extends React.Component {
     constructor() {
         super();
+
+        this.state = {
+            timestamp: 'no timestamp yet'
+        };
+    }
+
+    componentDidMount() {
+        subscribeToTimer((err, timestamp) => this.setState({
+            timestamp
+        }));
     }
 
     render() {
@@ -15,9 +27,11 @@ class About extends React.Component {
                         <h1>About <img className="Logo" src='/img/searchx_logo.png'/> </h1>
                         <div className="Info" >
                             <p>
-                                SearchX is a search interface developed by <a href="http://www.wis.ewi.tudelft.nl"> TU Delft's Web Information Systems group </a>.
+                                SearchX is a search interface developed by <a href="http://www.wis.ewi.tudelft.nl"> TU Delft's Web Information Systems group</a>.
                                 If you have questions or issues please contact <a href="http://www.wis.ewi.tudelft.nl/moraes/">Felipe Moraes</a> at f.moraes@tudelft.nl.
                             </p>
+
+                            This is the timer value: {this.state.timestamp}
                          </div>
 
                         <img className="TUDLogo" src='/img/TUDLogo.png'/>
