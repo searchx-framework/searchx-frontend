a003e44e8f8e5c44000712882520f3e1ba5bb3bc
sindunuragarp <sindunuragarp@gmail.com>
2018-01-10 00:09:13 +0100

    Added user's assigned name

--- src/js/components/Survey/Task/Task.js
+++ src/js/components/Survey/Task/Task.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 import TimedSubmit from './TimedSubmit';
 import TaskStore from '../../../stores/TaskStore';
+import AccountStore from "../../../stores/AccountStore";
 
 class Task extends React.Component {    
 
@@ -18,34 +19,40 @@ class Task extends React.Component {
         let start = localStorage.getItem("counter-start") || 0;
 
         return(
-            <div className="Task row" id={this.state.task.topicId}>
-                <div className="Task-title">
-                    Task Description
+            <div className="Task">
+                <div className="Task-box name" style={{backgroundColor: AccountStore.getMemberColor(AccountStore.getId())}}>
+                    Name: {AccountStore.getMemberName(AccountStore.getId())}
                 </div>
 
-                <div className="Task-info no-padding" id="intro-description">
-                    <div className="Task-instruction-specific">
-                        <div dangerouslySetInnerHTML={{__html: TaskStore.getTopicDescription(this.state.task.topicId)}} />
-                         
+                <div className="Task-box instruction" id={this.state.task.topicId}>
+                    <div className="Task-title">
+                        Task Description
                     </div>
-                
+
+                    <div className="Task-info no-padding" id="intro-description">
+                        <div className="Task-instruction-specific">
+                            <div dangerouslySetInnerHTML={{__html: TaskStore.getTopicDescription(this.state.task.topicId)}} />
+
+                        </div>
+
+                        <hr/>
+
+                        <div className="Task-instruction-general">
+                            The professor requires all students to demonstrate what they learn about a particular topic by conducting searches online and presenting their views on the topic. To prepare your term paper, you need to collect and save all the webpages, publications, and other online sources that are helpful for you to write a paper.
+
+                        <hr/>
+                        After you have completed the search phase, you will be asked to complete 13 exercises; those exercises include questions about your term paper topic and the writing of an outline for your term paper.
+                        </div>
+                        <div className="Task-instruction-general">
+                        </div>
                     <hr/>
 
-                    <div className="Task-instruction-general">
-                        The professor requires all students to demonstrate what they learn about a particular topic by conducting searches online and presenting their views on the topic. To prepare your term paper, you need to collect and save all the webpages, publications, and other online sources that are helpful for you to write a paper. 
-
-                    <hr/>
-                    After you have completed the search phase, you will be asked to complete 13 exercises; those exercises include questions about your term paper topic and the writing of an outline for your term paper.
                     </div>
-                    <div className="Task-instruction-general">
+
+
+                    <div className="Task-submit no-padding">
+                        <TimedSubmit start={start} duration={this.state.task.duration}/>
                     </div>
-                <hr/>
-
-                </div>
-
-
-                <div className="Task-submit no-padding">
-                    <TimedSubmit start={start} duration={this.state.task.duration}/>
                 </div>
             </div>
         )
