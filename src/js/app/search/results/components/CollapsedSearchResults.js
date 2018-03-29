c84b9325ba50ffa546755d7dd7be07f2706d3f7b
Kilian Grashoff <kiliangrashoff@gmail.com>
2018-03-29 13:31:14 +0200

    Collapse bookmarked results for soft distribution of labour

--- src/js/app/search/results/components/CollapsedSearchResults.js
+++ src/js/app/search/results/components/CollapsedSearchResults.js
@@ -1,9 +1,32 @@
-const CollapsedSearchResults = function({results}) {
-    return (
-        <div className="CollapsedSearchResults">
-            {results}
-        </div>
-    );
-};
+import React from 'react';
+import {Panel} from "react-bootstrap";
+
+class CollapsedSearchResults extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            open: false
+        };
+    }
+
+    render() {
+        return (
+            <Panel className="CollapsedSearchResults">
+                <Panel.Heading>
+                    <Panel.Title toggle>
+                        {this.props.results.length} previously bookmarked results hidden
+                    </Panel.Title>
+                </Panel.Heading>
+                <Panel.Collapse>
+                    <Panel.Body>
+                        {this.props.results}
+                    </Panel.Body>
+                </Panel.Collapse>
+            </Panel>
+        );
+
+    }
+}
 
 export default CollapsedSearchResults;
