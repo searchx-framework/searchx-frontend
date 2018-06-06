fee9156bf861419cf77088b65da7791e73efeb39
sindunuragarp <sindunuragarp@gmail.com>
2018-06-07 01:27:36 +0200

    Added checking of required env variables

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app/App";
 import registerServiceWorker from './registerServiceWorker';
+import {checkRequiredEnv} from './requireEnv';
 
 import Alert from "react-s-alert";
 import "react-s-alert/dist/s-alert-default.css";
@@ -15,5 +16,6 @@ const app = (
     </div>
 );
 
+checkRequiredEnv();
 ReactDOM.render(app, document.getElementById("root"));
 registerServiceWorker();
