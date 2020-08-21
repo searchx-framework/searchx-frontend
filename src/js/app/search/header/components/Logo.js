5bde87880f236b885bde0890f4ff177a8eba803d
Felipe Moraes <fe.moraesg@gmail.com>
2020-08-21 14:30:04 +0200

    tag dataset to logo

--- src/js/app/search/header/components/Logo.js
+++ src/js/app/search/header/components/Logo.js
@@ -1,10 +1,11 @@
 import './SearchHeader.pcss'
 import React from 'react';
-
+import config from '../../../../../js/config'
 const Logo = function() {
     return (
         <div className="logo">
             <div className="image"/>
+            <p className="dataset"> {config.dataset}</p>
         </div>
     )
 };
--- src/js/config.js
+++ src/js/config.js
@@ -41,6 +41,7 @@ const providerViewers = {
 
 
 var config = {
+    dataset: "ECOMM",
     aboutPrefixAt: 1000, /* at how many search results do we start saying 'About X results' vs. 'X results' */
     logTimeInterval: 5000,
     defaultProvider: 'elasticsearch',
