647077eae6c67fbcc5e4ab6de32570fa01a919a9
Felipe Moraes <fe.moraesg@gmail.com>
2021-03-17 16:51:52 +0100

    change task time and add description to between tasks

--- src/js/app/tasks/ecomm/TaskDescriptionShort.js
+++ src/js/app/tasks/ecomm/TaskDescriptionShort.js
@@ -6,7 +6,7 @@ import {LoggerEventTypes} from "../../../utils/LoggerEventTypes";
 import AccountStore from "../../../stores/AccountStore";
 import SearchStore from "../../search/SearchStore";
 import constants from "./constants";
-
+import {getTaskDescription} from "./Utils";
 import './Ecomm.pcss';
 import Timer from "../components/Timer";
 
@@ -74,11 +74,12 @@ class TaskDescription extends React.Component {
         localStorage.setItem("current-topic", t || 0);
         const groupId = AccountStore.getGroupId();
         AccountStore.setSessionId(groupId+"-"+ task.topics[t].id + "-" + SearchStore.getVariant() );
+        let taskDescription = getTaskDescription("decision");
         return ( <div className="Wait waitBox">
 
             
-            <p>Your job will be the same as before. However, we will give your group a new task.</p>
-
+            <p>Now that you have done the training task. Here is the main task of this study</p>
+            {taskDescription}
             <p> You will be redirected once the time is up! <b>DO NOT PRESS THE BROWSER BACK BUTTON!</b> This will invalidate your participation!</p>
             <Timer start={start} duration={constants.taskDescriptionWait} onFinish={this.onFinish} style={{fontSize: '2em'}} showRemaining={true}/>
         </div>
