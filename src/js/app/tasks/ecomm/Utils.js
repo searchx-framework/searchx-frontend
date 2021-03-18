bf8bc15c3ed55480c8d84ba86c163681d26d3fca
Felipe Moraes <fe.moraesg@gmail.com>
2021-03-18 10:42:06 +0100

    add paragraphs to task description

--- src/js/app/tasks/ecomm/Utils.js
+++ src/js/app/tasks/ecomm/Utils.js
@@ -40,13 +40,16 @@ const decisionTaskDescription = () => (
     <p>
         Alex and Charlie, a young couple who are your friends, just moved in together, and they have invited your and your group members to a housewarming party. 
         Your group members have decided to buy a gift together for your friends. 
-        Together, you should decide on a budget in the range of $50,00 and $200,00. 
+        Together, you should decide on a budget in the range of $50,00 and $200,00.  </p>
+     <p>
         You don't know yet what to buy, but you should make sure that you choose a gift that both Alex and Charlie will enjoy. 
         You are not sure whether to give them an electronic product since Alex is a gadget nerd or to help one of them improve their home workspace since Charlie complained to your group recently about their sub-optimal working-from-home setup. 
-        Perhaps, Alex would like some sports accessories given their passion for outdoor sports. 
+        Perhaps, Alex would like some sports accessories given their passion for outdoor sports.  </p>
+    <p>
         Together with your group members, decide what to buy to bring with you to the housewarming party. 
         Using our search system, find one product you think your friends will like. 
-        To do so, first your group should explore a number of different products and save the ones you find good candidates to give as a gift in the <em>Saved Items</em> widget. 
+        To do so, first your group should explore a number of different products and save the ones you find good candidates to give as a gift in the <em>Saved Items</em> widget. </p>
+    <p>
         After you have found a few candidates, discuss which one to settle on as the final gift to buy. One person of the group should put the final item your group decided to buy from the <em>Saved Items</em> widget to the <em>Shopping Basket</em> widget. 
         You want to buy a good gift but are also pressed for time as the party will start in an hour. You have thus 30 minutes to find a gift for your friends. 
     </p>
